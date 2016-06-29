11feeb219dc1838eb055ef23f9f753c84358160b
n-white <neil.r.whitehead@gmail.com>
2016-06-29 15:38:45 -0700

    Implement readFile on sietAssets and archivedSites in a GET request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,46 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httphelpers = require('./http-helpers');
+
+var fs = require('fs');
+
+var headers = httphelpers.headers;
+
+var results = '';
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  
+  var statusCode;
+
+
+  if (req.method === 'GET') {
+    statusCode = 200;
+
+    var url;
+
+    if (req.url === '/') {
+      url = '/index.html';
+      fs.readFile(archive.paths.siteAssets + url, { encoding: 'utf-8' }, (err, data) => {
+        if (err) throw err;
+        results = data;
+      });
+    } else {
+      url = req.url;
+      fs.readFile(archive.paths.archivedSites + url, { encoding: 'utf-8' }, (err, data) => {
+        if (err) throw err;
+        results = data;
+        // console.log(data);
+      });
+    }
+
+
+    res.writeHead(statusCode, headers);
+    console.log(results);
+    res.end(results);
+  }
+
+
+  //res.end(archive.paths.list);
+  
 };
